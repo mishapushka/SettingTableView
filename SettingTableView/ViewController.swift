9bd9777f3212280fb8888982c91549ca5b7abb71
Mikhail <bskfrost3@yandex.ru>
2021-05-08 13:36:03 +0700

    Initial Commit

---
+++ SettingTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SettingTableView
+//
+//  Created by mac on 08.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
